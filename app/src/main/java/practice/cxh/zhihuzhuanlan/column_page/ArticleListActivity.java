7ae8eef2578894aafe375283a256440666826511
chenxiaohan <1356957916@qq.com>
2018-09-04 22:49:38 +0800

    专栏主页使用折叠式标题栏

--- app/src/main/java/practice/cxh/zhihuzhuanlan/column_page/ArticleListActivity.java
+++ app/src/main/java/practice/cxh/zhihuzhuanlan/column_page/ArticleListActivity.java
@@ -4,15 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.support.v7.widget.Toolbar;
 
 import practice.cxh.zhihuzhuanlan.R;
-import practice.cxh.zhihuzhuanlan.entity.ArticleEntity;
 import practice.cxh.zhihuzhuanlan.entity.ColumnEntity;
 
 public class ArticleListActivity extends AppCompatActivity {
@@ -20,6 +16,7 @@ public class ArticleListActivity extends AppCompatActivity {
     public static String COLUMN_ENTITY = "column_entity";
 
     private ArticleListFragment mArticleListFragment;
+    private Toolbar tbArticleListPage;
 
     public static void lauch(Activity activity, ColumnEntity columnEntity) {
         Intent intent = new Intent(activity, ArticleListActivity.class);
@@ -35,7 +32,17 @@ public class ArticleListActivity extends AppCompatActivity {
 
     private void initView() {
         setContentView(R.layout.activity_article_list_wrap);
+        tbArticleListPage = findViewById(R.id.tb_article_list);
+        setSupportActionBar(tbArticleListPage);
+//        ColumnEntity columnEntity = (ColumnEntity) getIntent().getSerializableExtra(COLUMN_ENTITY);
         mArticleListFragment = new ArticleListFragment();
         getSupportFragmentManager().beginTransaction().add(R.id.container, mArticleListFragment).commit();
+    }
+
+    public void setToolbarTitle(String title) {
+        ActionBar actionBar = this.getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(title);
+        }
     }
 }
