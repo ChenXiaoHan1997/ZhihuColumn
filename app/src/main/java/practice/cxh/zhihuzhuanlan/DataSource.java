813f4efd1e5fdd8eab0e6b8cd6bf7c5b7bf89bb6
chenxiaohan <1356957916@qq.com>
2018-09-12 10:51:02 +0800

    下载页显示篇数

--- app/src/main/java/practice/cxh/zhihuzhuanlan/DataSource.java
+++
@@ -1,9 +0,0 @@
-package practice.cxh.zhihuzhuanlan;
-
-import java.util.List;
-
-public interface DataSource<T> {
-    T getDataAt(int position);
-    List<T> getDataWithRange(int start, int count);
-    int getItemCount();
-}
