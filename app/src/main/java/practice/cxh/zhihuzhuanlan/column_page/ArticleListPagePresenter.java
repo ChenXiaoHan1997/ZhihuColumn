33a87a9d64a59e439299dff36041c58e8b366869
chenxiaohan <1356957916@qq.com>
2018-09-12 10:34:15 +0800

    专栏主页首次加载30项

--- app/src/main/java/practice/cxh/zhihuzhuanlan/column_page/ArticleListPagePresenter.java
+++ app/src/main/java/practice/cxh/zhihuzhuanlan/column_page/ArticleListPagePresenter.java
@@ -19,6 +19,7 @@ import practice.cxh.zhihuzhuanlan.util.JsonUtil;
 public class ArticleListPagePresenter {
 
     private static final int ARTICLE_LIMIT = 10;
+    private static final int FIRST_LOAD_LIMIT = 30;
 
     private ArticleListV mArticleListV;
     private Handler mUiHandler;
@@ -30,34 +31,36 @@ public class ArticleListPagePresenter {
 
     public void loadArticleList(final String columnSlug, final int offset) {
         // TODO 先从数据库加载
-        HttpUtil.get(HttpUtil.API_BASE + HttpUtil.COLUMN + "/" + columnSlug + "/" + HttpUtil.POSTS + "?offset=" + offset,
+        HttpUtil.get(HttpUtil.API_BASE + HttpUtil.COLUMN + "/" + columnSlug
+                        + "/" + HttpUtil.POSTS + "?offset=" + offset
+                        + "&limit=" + FIRST_LOAD_LIMIT,
                 new HttpUtil.HttpListener<String>() {
-            @Override
-            public void onSuccess(String response) {
-                List<Article> articlesList = JsonUtil.decodeArticleList(response);
-                List<ArticleEntity> articleEntityList = new ArrayList<ArticleEntity>();
-                for (Article article : articlesList) {
-                    // articleEntity是新获得的
-                    ArticleEntity articleEntity = ArticleEntity.convertFromArticle(article, columnSlug);
-                    // 若数据库中已有此文章，设置articleEntity的下载状态
-                    List<ArticleEntity> tmp = DbUtil.getArticleEntityDao()
-                            .queryBuilder()
-                            .where(ArticleEntityDao.Properties.Slug.eq(articleEntity.getSlug()))
-                            .list();
-                    if (tmp.size() > 0) {
-                        articleEntity.setDownloadState(tmp.get(0).getDownloadState());
+                    @Override
+                    public void onSuccess(String response) {
+                        List<Article> articlesList = JsonUtil.decodeArticleList(response);
+                        List<ArticleEntity> articleEntityList = new ArrayList<ArticleEntity>();
+                        for (Article article : articlesList) {
+                            // articleEntity是新获得的
+                            ArticleEntity articleEntity = ArticleEntity.convertFromArticle(article, columnSlug);
+                            // 若数据库中已有此文章，设置articleEntity的下载状态
+                            List<ArticleEntity> tmp = DbUtil.getArticleEntityDao()
+                                    .queryBuilder()
+                                    .where(ArticleEntityDao.Properties.Slug.eq(articleEntity.getSlug()))
+                                    .list();
+                            if (tmp.size() > 0) {
+                                articleEntity.setDownloadState(tmp.get(0).getDownloadState());
+                            }
+                            articleEntityList.add(articleEntity);
+                        }
+                        mArticleListV.onArticleListLoaded(articleEntityList, false);
+                        saveArticleList(articleEntityList);
                     }
-                    articleEntityList.add(articleEntity);
-                }
-                mArticleListV.onArticleListLoaded(articleEntityList, false);
-                saveArticleList(articleEntityList);
-            }
 
-            @Override
-            public void onFail(String detail) {
-                loadArticleListFromDB(columnSlug);
-            }
-        });
+                    @Override
+                    public void onFail(String detail) {
+                        loadArticleListFromDB(columnSlug);
+                    }
+                });
     }
 
     private void saveArticleList(final List<ArticleEntity> articleEntityList) {
@@ -73,6 +76,7 @@ public class ArticleListPagePresenter {
 
     /**
      * 从数据库中加载所有的文章
+     *
      * @param columnSlug
      */
     private void loadArticleListFromDB(final String columnSlug) {
@@ -81,10 +85,11 @@ public class ArticleListPagePresenter {
 
     /**
      * 从数据库中加载文章
+     *
      * @param columnSlug 专栏slug
-     * @param offset 偏移
-     * @param limit 最多加载数量
-     * @param clearOld 清除UI上旧的列表
+     * @param offset     偏移
+     * @param limit      最多加载数量
+     * @param clearOld   清除UI上旧的列表
      */
     private void loadArticleListFromDB(final String columnSlug, final int offset, final int limit, final boolean clearOld) {
         AsyncUtil.getThreadPool().execute(new Runnable() {
