3aae616f737599525de3abef99f07c8352bd3c36
chenxiaohan <1356957916@qq.com>
2018-09-13 18:15:50 +0800

    专栏主页先从数据库加载，等网络请求成功后再局部刷新

--- app/src/main/java/practice/cxh/zhihuzhuanlan/column_page/ArticleListPagePresenter.java
+++ app/src/main/java/practice/cxh/zhihuzhuanlan/column_page/ArticleListPagePresenter.java
@@ -18,8 +18,7 @@ import practice.cxh.zhihuzhuanlan.util.JsonUtil;
 
 public class ArticleListPagePresenter {
 
-    private static final int ARTICLE_LIMIT = 10;
-    private static final int FIRST_LOAD_LIMIT = 30;
+
 
     private ArticleListV mArticleListV;
     private Handler mUiHandler;
@@ -29,15 +28,15 @@ public class ArticleListPagePresenter {
         mUiHandler = new Handler();
     }
 
-    public void loadArticleList(final String columnSlug, final int offset) {
+    public void loadArticleList(final String columnSlug, final int offset, int limit) {
         // TODO 先从数据库加载
+        loadArticleListFromDB(columnSlug, offset, limit, false);
         HttpUtil.get(HttpUtil.API_BASE + HttpUtil.COLUMN + "/" + columnSlug
                         + "/" + HttpUtil.POSTS + "?offset=" + offset
-                        + "&limit=" + FIRST_LOAD_LIMIT,
+                        + "&limit=" + limit,
                 new HttpUtil.HttpListener<String>() {
                     @Override
                     public void onSuccess(String response) {
-                        // TODO 解码等操作放到子线程
                         List<Article> articlesList = JsonUtil.decodeArticleList(response);
                         List<ArticleEntity> articleEntityList = new ArrayList<ArticleEntity>();
                         for (Article article : articlesList) {
@@ -53,7 +52,7 @@ public class ArticleListPagePresenter {
                             }
                             articleEntityList.add(articleEntity);
                         }
-                        mArticleListV.onArticleListLoaded(articleEntityList, false);
+                        mArticleListV.onArticleListLoaded(articleEntityList, offset);
                         saveArticleList(articleEntityList);
                     }
 
