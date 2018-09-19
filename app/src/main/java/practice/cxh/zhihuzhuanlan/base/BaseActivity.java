1ab329da0900d1f5422c6dfb6f2d127b986b143b
chenxiaohan <1356957916@qq.com>
2018-09-19 14:48:39 +0800

    BasePresenter

---
+++ app/src/main/java/practice/cxh/zhihuzhuanlan/base/BaseActivity.java
@@ -0,0 +1,7 @@
+package practice.cxh.zhihuzhuanlan.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class BaseActivity<V, P extends BasePresenter<V>> extends AppCompatActivity {
+    
+}
---
+++ app/src/main/java/practice/cxh/zhihuzhuanlan/base/BasePresenter.java
@@ -0,0 +1,27 @@
+package practice.cxh.zhihuzhuanlan.base;
+
+import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
+
+public abstract class BasePresenter<V> {
+    protected Reference<V> mViewRef;    // View
+
+    public void attachView(V view) {
+        mViewRef = new WeakReference<V>(view);
+    }
+
+    protected V getView() {
+        return mViewRef.get();
+    }
+
+    public boolean isViewAttached() {
+        return mViewRef != null && mViewRef.get() != null;
+    }
+
+    public void detachView() {
+        if (mViewRef != null) {
+            mViewRef.clear();
+            mViewRef = null;
+        }
+    }
+}
